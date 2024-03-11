9628a9d591349c5343aa19cd8cefab3356b0f5bd
Niischay4 <147917000+Niischay7@users.noreply.github.com>
2024-03-11 23:54:27 +0530

    Update assembler.py

--- assembler.py
+++ assembler.py
@@ -71,5 +71,31 @@ list_J=["jal"]
 
 
 register_encoding = {"zero": "00000","ra":"00001","sp":"00010","gp":"00011","tp":"00100","t0":"00101","t1":"00110","t2":"00111","s0":"01000","fp":"01000","s1":"01001","a0":"01010","a1":"01011","a2":"01100","a3":"01101","a4":"01110","a5":"01111","a6":"10000","a7":"10001","s2":"10010","s3":"10011","s4":"10100","s5":"10101","s6":"10110","s7":"10111","s8":"11000","s9":"11001","s10":"11010","s11":"11011","t3":"11100","t4":"11101","t5":"11110","t6":"11111"}
+def func_U(instruction):
+    global program_counter
+    try:
 
+        answer=""
+        imm_bin=decimal_binary_32bits(instruction[2])
+        answer= imm_bin[:-11]+register_encoding[instruction[1]] +opcode_U[instruction[0]]
+        program_counter+=4
+        return answer
+    except: 
+        global program_counter
+        flag=1
+        error1="no operation belongs to the above code snippet error in"
+        pc=program_counter
+        
+def func_R(instruction):
+    global program_counter
+    try:
+        answer = ""
+        answer=R_func7[instruction[0]]+register_encoding[instruction[3]]+register_encoding[instruction[2]]+R_func3[instruction[0]]+register_encoding[instruction[1]]+opcode_R[instruction[0]]
+        program_counter+=4
+        return answer
+    except:
+        global program_counter
+        flag=1
+        error1="no operation belongs to the above code snippet error in"
+        pc=program_counter
 
