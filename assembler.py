2136f75df0251c979248f837b8f263a984533c89
Niischay4 <147917000+Niischay7@users.noreply.github.com>
2024-03-12 00:00:08 +0530

    Update assembler.py

--- assembler.py
+++ assembler.py
@@ -204,3 +204,58 @@ if(not bool(dict)):
             list1.append(func_U(instruction_s))
         elif instruction_s[0] in list_J:
             list1.append(func_J(instruction_s))
+else:
+    program_counter=0
+    cnt=0
+    for instruction_i in instruction_processed:
+        if(instruction_i[-1] in list(dict.keys())):
+            dict2[program_counter]=instruction_i[-1]
+            program_counter+=4
+        else:
+            program_counter+=4
+    
+
+    for instruction_s in instruction_processed:
+        if instruction_s[0] in list_R:
+            if(instruction_s[-1] in list(dict.keys()) and cnt in list(dict2.keys())):
+                instruction_s[-1]=str(dict[instruction_s[-1]]-cnt)
+                list1.append(func_R(instruction_s))
+            else:   
+                list1.append(func_R(instruction_s))
+            cnt+=4
+        elif instruction_s[0] in list_I:
+            if(instruction_s[-1] in list(dict.keys()) and cnt in list(dict2.keys())):
+                instruction_s[-1]=str(dict[instruction_s[-1]]-cnt)
+                list1.append(func_I(instruction_s))
+            else:   
+                list1.append(func_I(instruction_s))
+            cnt+=4
+        elif instruction_s[0] in list_B:
+            if(instruction_s[-1] in list(dict.keys()) and cnt in list(dict2.keys())):
+                instruction_s[-1]=str(dict[instruction_s[-1]]-cnt)
+                list1.append(func_B(instruction_s))
+            else:   
+                list1.append(func_B(instruction_s))
+            cnt+=4
+        elif instruction_s[0] in list_S:
+            if(instruction_s[-1] in list(dict.keys()) and cnt in list(dict2.keys())):
+                instruction_s[-1]=str(dict[instruction_s[-1]]-cnt)
+                list1.append(func_S(instruction_s))
+            else:   
+                list1.append(func_S(instruction_s))
+            cnt+=4
+        elif instruction_s[0] in list_U:
+            if(instruction_s[-1] in list(dict.keys()) and cnt in list(dict2.keys())):
+                instruction_s[-1]=str(dict[instruction_s[-1]]-cnt)
+                list1.append(func_U(instruction_s))
+            else:   
+                list1.append(func_U(instruction_s))
+            cnt+=4
+        elif instruction_s[0] in list_J:
+            if(instruction_s[-1] in list(dict.keys()) and cnt in list(dict2.keys())):
+                instruction_s[-1]=str(dict[instruction_s[-1]]-cnt)
+                list1.append(func_J(instruction_s))
+            else:   
+                list1.append(func_J(instruction_s))
+            cnt+=4
+
